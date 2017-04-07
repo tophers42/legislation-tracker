4545a8843400513210d6dd42cc1de0e1b3bb863f
Christoph Neyer <tophers@Christophs-MacBook-Pro.local>
2017-04-06 22:29:32 -0700

    Initial models

--- legislation_tracker/settings.py
+++ legislation_tracker/settings.py
@@ -73,15 +73,15 @@ WSGI_APPLICATION = 'legislation_tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
- DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql',
-         'NAME': 'postgres',
-         'USER': 'postgres',
-         'HOST': 'db',
-         'PORT': 5432,
-     }
- }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
 
 
 # Password validation
